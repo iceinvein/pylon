b262d9ec56ad6b8a0bf7a35783bef96e2f2694b4
Dik Rana <dik.rana@mechanicalrock.io>
2026-03-09 11:51:02 +0800

    feat(pr-review): add PrReviewView page

---
+++ src/renderer/src/pages/PrReviewView.tsx
@@ -0,0 +1,32 @@
+import { useEffect } from 'react'
+import { usePrReviewStore } from '../store/pr-review-store'
+import { PrList } from '../components/pr-review/PrList'
+import { PrDetail } from '../components/pr-review/PrDetail'
+import { GhSetupGuide } from '../components/pr-review/GhSetupGuide'
+
+export function PrReviewView() {
+  const { ghStatus, checkGhStatus } = usePrReviewStore()
+
+  useEffect(() => {
+    checkGhStatus()
+  }, [])
+
+  const isReady = ghStatus?.available && ghStatus?.authenticated
+
+  return (
+    <div className="flex h-full">
+      {isReady ? (
+        <>
+          <div className="w-[280px] flex-shrink-0">
+            <PrList />
+          </div>
+          <div className="min-w-0 flex-1">
+            <PrDetail />
+          </div>
+        </>
+      ) : (
+        <GhSetupGuide />
+      )}
+    </div>
+  )
+}
