374ba1f109db789ca6bbc376b2fbf4d807fca83e
Dik Rana <dik.rana@mechanicalrock.io>
2026-03-13 12:06:08 +0800

    feat(bridge): add goal suggestion IPC subscription to test bridge

--- src/renderer/src/hooks/use-test-bridge.ts
+++ src/renderer/src/hooks/use-test-bridge.ts
@@ -1,13 +1,22 @@
 import { useEffect } from 'react'
+import type { ExplorationUpdate, GoalSuggestionUpdate } from '../../../shared/types'
 import { useTestStore } from '../store/test-store'
 
 export function useTestBridge() {
   const handleExplorationUpdate = useTestStore((s) => s.handleExplorationUpdate)
+  const handleGoalSuggestion = useTestStore((s) => s.handleGoalSuggestion)
 
   useEffect(() => {
     const unsub = window.api.onExplorationUpdate((data) => {
-      handleExplorationUpdate(data as Parameters<typeof handleExplorationUpdate>[0])
+      handleExplorationUpdate(data as ExplorationUpdate)
     })
     return unsub
   }, [handleExplorationUpdate])
+
+  useEffect(() => {
+    const unsub = window.api.onGoalSuggestion((data) => {
+      handleGoalSuggestion(data as GoalSuggestionUpdate)
+    })
+    return unsub
+  }, [handleGoalSuggestion])
 }
